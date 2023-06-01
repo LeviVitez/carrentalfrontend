69f24c76c0f9c0863b709b6917fdb55277c7e72e
LeviVitez <bmwyolo555@gmail.com>
2023-06-01 18:50:24 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Main from './pages/Main';
+
+function App() {
+  return (
+    <Main></Main>
+  );
+}
+
+export default App;
